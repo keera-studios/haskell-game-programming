47bc995e779ef3e9e8c21f06bfcd459d156d52b1
Ivan Perez <ivan.perez@keera.co.uk>
2015-09-27 20:30:32 +0200

    A few more complicated drawings

---
+++ tutorials/sdl/sdl1/drawings/SophistiShapes.hs
@@ -0,0 +1,51 @@
+
+import Data.IORef 
+import Control.Monad
+import Graphics.UI.SDL as SDL
+import Graphics.UI.SDL.Primitives as SDL
+import Graphics.UI.SDL.Image as SDL
+
+main :: IO ()
+main = do
+  SDL.init [InitVideo]
+
+  screen <- SDL.setVideoMode 1024 768 32 [SWSurface]
+  let format = SDL.surfaceGetPixelFormat screen
+  white <- SDL.mapRGB format 0xFF 0xFF 0xFF
+
+  diffX <- newIORef 0
+
+  forever $ do
+    SDL.fillRect screen Nothing white
+
+    filledRectangleRR screen 180 30 240 100 20 (Pixel 0xFF0000FF)
+    
+    rectangleRR screen 180 150 240 180 15 (Pixel 0xFF0000FF)
+
+    filledRectangleRR screen 180 230 440 260 15 (Pixel 0x00FF00FF)
+    rectangleRR       screen 180 230 440 260 15 (Pixel 0xFF0000FF)
+    rectangleRR       screen 181 230 439 260 15 (Pixel 0xFF0000FF)
+    rectangleRR       screen 180 231 440 259 15 (Pixel 0xFF0000FF)
+
+    SDL.flip screen
+
+    SDL.delay 10
+
+rectangleRR surface x1 y1 x2 y2 rad pixel = do
+    hLine surface (x1 + rad) (x2 - rad) y1 pixel
+    hLine surface (x1 + rad) (x2 - rad) y2 pixel
+    vLine surface x1 (y1 + rad) (y2 - rad) pixel
+    vLine surface x2 (y1 + rad) (y2 - rad) pixel
+    arc surface (x1 + rad) (y1 + rad) rad 180 270 pixel
+    arc surface (x2 - rad) (y1 + rad) rad 270 360 pixel
+    arc surface (x1 + rad) (y2 - rad) rad 090 180 pixel
+    arc surface (x2 - rad) (y2 - rad) rad 000 090 pixel
+
+filledRectangleRR surface x1 y1 x2 y2 rad pixel = do
+    box surface (Rect (fI $ x1 + rad) (fI $ y1)       (fI $ x2 - rad) (fI y2))         pixel
+    box surface (Rect (fI x1)         (fI $ y1 + rad) (fI x2)         (fI $ y2 - rad)) pixel
+    filledPie surface (x1 + rad) (y1 + rad) rad 180 270 pixel
+    filledPie surface (x2 - rad) (y1 + rad) rad 270 360 pixel
+    filledPie surface (x1 + rad) (y2 - rad) rad 090 180 pixel
+    filledPie surface (x2 - rad) (y2 - rad) rad 000 090 pixel
+ where fI = fromIntegral
